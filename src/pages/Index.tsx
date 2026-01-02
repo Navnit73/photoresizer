cb72fbbb4b92a5368f4cd514f66a54340a5b5b46
Navnit Rai <navnitrai5389@gmail.com>
2026-01-02 23:07:26 +0530

    Remove BookmarkPopup component from Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -36,7 +36,7 @@ const Index = () => {
   return (
     <>
       <SEO />
-      <BookmarkPopup />
+    
 
       <div className="min-h-screen flex flex-col bg-slate-50 dark:bg-slate-900">
         <Header />
