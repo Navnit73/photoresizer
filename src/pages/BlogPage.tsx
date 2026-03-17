775474b237e7af7f52f29d8dc6a27ebbdaa6a8fc
Navnit Rai <navnitrai5389@gmail.com>
2026-03-17 19:24:38 +0530

    feat: add BlogPage component for displaying individual blog posts with dynamic content, SEO, and UI elements.

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -176,9 +176,9 @@ export default function BlogPage() {
             </div>
 
             {/* AdSense Unit - Top of Content */}
-            <div className="mb-10">
+            {/* <div className="mb-10">
                <AdUnit />
-            </div>
+            </div> */}
 
             {/* SPECIFICATION CARDS - DYNAMIC RENDERING */}
             {post.specifications && (
